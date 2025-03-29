0df271772011de1ada512b623618f72e56884000
= <=>
2025-03-29 14:48:43 +1300

    homogonising list

---
+++ assets/js/activityData.js
@@ -0,0 +1,509 @@
+    const activityLists = {
+    drawing: {
+        play: [
+            {
+                title: "Draw the Aviary alongside book",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Sketch random objects in room",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw a previous creature from imagination",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw Plant alongside Book",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw Insect alongside Book",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw Bone Structure alongside Book",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw Gesture alongside Book",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw Land Animal alongside Book",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw Marine alongside Book",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw Face with reference",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw expressive heads and shoulders from imagination",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw hands from imagination",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw skulls from imagination",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Draw manequins from imagination",
+                url: "",
+                variables: []
+            }
+        ],
+        practice: [
+            {
+                title: "Drawing Exercise Lvl1",
+                url: "https://www.notion.so/Drawing-Exercises-1ac84569b31a806f8b94e0be66acc0ec?pvs=4",
+                variables: []
+            },
+            {
+                title: "Drawing Exercise Lvl2",
+                url: "https://www.notion.so/Drawing-Exercises-1ac84569b31a806f8b94e0be66acc0ec?pvs=4",
+                variables: []
+            },
+            {
+                title: "Drawing Exercise Lvl3",
+                url: "https://www.notion.so/Drawing-Exercises-1ac84569b31a806f8b94e0be66acc0ec?pvs=4",
+                variables: []
+            },
+            {
+                title: "Drawing Exercise Lvl4",
+                url: "https://www.notion.so/Drawing-Exercises-1ac84569b31a806f8b94e0be66acc0ec?pvs=4",
+                variables: []
+            },
+            {
+                title: "Drawing Exercise Lvl5",
+                url: "https://www.notion.so/Drawing-Exercises-1ac84569b31a806f8b94e0be66acc0ec?pvs=4",
+                variables: []
+            },
+            {
+                title: "explore cross hatching for lighting and texture",
+                url: "",
+                variables: []
+            },
+            {
+                title: "explore soluettes for buildings",
+                url: "",
+                variables: []
+            },
+            {
+                title: "explore soluettes for characters",
+                url: "",
+                variables: []
+            },
+            {
+                title: "explore soluettes for scenery/ nature",
+                url: "",
+                variables: []
+            }
+        ]
+    },
+    guitar: {
+        play: [
+            {
+                title: "Song: Steep Road",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Good Riddance",
+                url: "https://www.justinguitar.com/songs/green-day-good-riddance-time-of-your-life-chords-tabs-guitar-lesson-sg-066",
+                variables: []
+            },
+            {
+                title: "brown eyed girl",
+                url: "https://www.justinguitar.com/songs/van-morrison-brown-eyed-girl-chords-tabs-guitar-lesson-bs-304#learn-more",
+                variables: []
+            },
+            {
+                title: "Patience",
+                url: "https://www.justinguitar.com/songs/guns-n-roses-patience-1-3-easy-chords-tabs-guitar-lesson-st-621",
+                variables: []
+            },
+            {
+                title: "Wish you were here",
+                url: "https://www.justinguitar.com/guitar-lessons/wish-you-were-here-riff-for-beginners-b2-808",
+                variables: []
+            }
+        ],
+        practice: [
+            {
+                title: "Strumming patterns at speed in 4/4",
+                url: "https://www.justinguitar.com/guitar-lessons/the-strumming-pattern-b1-404",
+                variables: []
+            },
+            {
+                title: "C major scale",
+                url: "https://www.justinguitar.com/guitar-lessons/c-major-scale-b2-905",
+                variables: []
+            },
+            {
+                title: "all down 8th note strumming",
+                url: "https://www.justinguitar.com/guitar-lessons/all-down-8th-note-strumming-b2-805",
+                variables: []
+            },
+            {
+                title: "Chord transition practice Muting inbetween",
+                url: "",
+                variables: []
+            },
+            {
+                title: "E minor pentatonic with muting",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Strumming patterns at speed in 6/8",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Add to chord book/ charts",
+                url: "",
+                variables: []
+            }
+        ]
+    },
+    piano: {
+        play: [
+            {
+                title: "Song: Shan's Mood",
+                url: "https://www.jazzskills.com/products/developing-fluency/categories/2147575760/posts/16289729",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Song: I can't Beleive That You're In Love With Me",
+                url: "https://www.jazzskills.com/products/developing-fluency/categories/3558162/posts/11358218",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Song: This Love Of Mine",
+                url: "https://www.jazzskills.com/products/developing-fluency/categories/3558029/posts/11358157",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Song: Falling In Love With Love",
+                url: "https://www.jazzskills.com/products/developing-fluency/categories/3557819/posts/11358060",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Song: I'm In The Mood For Love",
+                url: "https://www.jazzskills.com/products/developing-fluency/categories/3557805/posts/11357949",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Song: Fly Me To The Moon",
+                url: "https://www.jazzskills.com/products/developing-fluency/categories/3557079/posts/11357434",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Song: Autumn Leaves",
+                url: "https://www.jazzskills.com/products/developing-fluency/categories/3557077/posts/11357433",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            }
+        ],
+        practice: [
+            {
+                title: "Inversions Practice",
+                url: "https://www.jazzskills.com/products/members-area/categories/2153428453/posts/2185299134",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Progressions Summary",
+                url: "https://www.jazzskills.com/products/developing-fluency/categories/2147570802/posts/11278365",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Rhythm Practice",
+                url: "https://www.jazzskills.com/products/members-area/categories/2148228334/posts/2169863863",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Dominant Voicing 1 I VI7 II V",
+                url: "https://www.jazzskills.com/products/members-area/categories/2153032196/posts/2168343519",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Dominant Voicing 1 II V I",
+                url: "https://www.jazzskills.com/products/members-area/categories/2153032196/posts/2168332764",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            },
+            {
+                title: "Major7 Voicings I VI II V",
+                url: "https://www.jazzskills.com/products/members-area/categories/2153032196/posts/2168307079",
+                variables: ["C", "F", "G", "Bb", "Eb"]
+            }
+        ]
+    },
+    comics: {
+        play: [
+            {
+                title: "Character brainstorming",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "Spontaneous story board sketching",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "Create an object for a world",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "Interaction brainstorm",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "Creative writing for world-building",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "draw a 4 panel comic",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "title page for comic",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            }
+        ],
+        practice: [
+            {
+                title: "Simple Ideas for a comic",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "Thumbnail and rough expression of a prior idea",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "title page thumbnail for comic",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "drawing thumbnail scene from different angles",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "pacing, how little words can you add",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "pacing, how many words can you add",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "effect of panel shape and placement",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            },
+            {
+                title: "Inking and shading exercise",
+                url: "",
+                variables: ["Fire", "Earth", "Ice", "Water", "Air", "Underground"]
+            }
+        ]
+    },
+    composition: {
+        play: [
+            {
+                title: "Free musical exploration",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Experimental sound mixing",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Create an 8 bar melody",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Improvise with different instruments",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Sound texture experimentation",
+                url: "",
+                variables: []
+            }
+        ],
+        practice: [
+            {
+                title: "Harmony study",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Rhythm pattern development",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Orchestration techniques",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Melodic variation exercises",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Composition structure analysis",
+                url: "",
+                variables: []
+            }
+        ]
+    },
+    singing: {
+        play: [
+            {
+                title: "Vocal improvisation",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Explore vocal range",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Sing without constraints",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Create vocal sounds",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Emotion-based singing",
+                url: "",
+                variables: []
+            }
+        ],
+        practice: [
+            {
+                title: "Breathing techniques",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Pitch precision exercises",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Vocal warm-ups",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Articulation practice",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Range expansion drills",
+                url: "",
+                variables: []
+            }
+        ]
+    },
+    rap: {
+        play: [
+            {
+                title: "Freestyle rap",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Rhythm exploration",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Word association game",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Spontaneous rhyming",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Flow experimentation",
+                url: "",
+                variables: []
+            }
+        ],
+        practice: [
+            {
+                title: "Breath control",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Rhyme scheme study",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Pronunciation clarity",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Rhythm precision",
+                url: "",
+                variables: []
+            },
+            {
+                title: "Lyric writing techniques",
+                url: "",
+                variables: []
+            }
+        ]
+    }
+};
---
+++ assets/js/timer-service-worker.js
@@ -0,0 +1,34 @@
+// timer-service-worker.js
+self.addEventListener('install', function(event) {
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', function(event) {
+    event.waitUntil(clients.claim());
+  });
+  
+  self.addEventListener('push', function(event) {
+    const title = 'Timer Completed!';
+    const options = {
+      body: 'Your 10-minute timer has completed!',
+      icon: 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2NCIgaGVpZ2h0PSI2NCIgdmlld0JveD0iMCAwIDI0IDI0IiBmaWxsPSJub25lIiBzdHJva2U9IiM0Q0FGNTAiIHN0cm9rZS13aWR0aD0iMiIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIiBzdHJva2UtbGluZWpvaW49InJvdW5kIj48Y2lyY2xlIGN4PSIxMiIgY3k9IjEyIiByPSIxMCI+PC9jaXJjbGU+PHBvbHlsaW5lIHBvaW50cz0iMTIgNiAxMiAxMiAxNiAxNCI+PC9wb2x5bGluZT48L3N2Zz4=',
+      vibrate: [200, 100, 200]
+    };
+  
+    event.waitUntil(
+      self.registration.showNotification(title, options)
+    );
+  });
+  
+  self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    
+    event.waitUntil(
+      clients.matchAll({type: 'window'}).then(function(clientList) {
+        if (clientList.length > 0) {
+          return clientList[0].focus();
+        }
+        return clients.openWindow('/');
+      })
+    );
+  });
